9432bba4c7f3bb443cc3e16fa85c73bbcbab2865
khenm <93570980+khenm@users.noreply.github.com>
2024-07-16 22:05:19 +0700

    Delete Module_1/Week-2/search.py

--- Module_1/Week-2/search.py
+++
@@ -1,9 +0,0 @@
-def Searching:
-    def binary_search(self, arr, key):
-        """Search with binary algorithm by key
-
-        Args:
-            arr (_type_): _description_
-            key (_type_): _description_
-        """
-        
